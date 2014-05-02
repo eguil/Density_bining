774fcf4069638de4d4e6e3a8c321e092cec85bcf
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2014-05-02 14:04:45 -0700

    add support_density.py

---
+++ support_density.py
@@ -0,0 +1,8 @@
+#!/Users/ericg/Projets/CMIP/Metrics/WGNE/bin/python
+# 
+#  ---------------------------------------
+#  Support python procs for density bining
+#  ---------------------------------------
+#  EG May 2014
+#
+#
