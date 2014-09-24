5b8f3e353bbea533a77c016c5cef1406fffbcead
Paul J. Durack <durack1@llnl.gov>
2014-09-24 16:29:54 -0700

    Commented depthBini and other variable delete

--- binDensity.py
+++ binDensity.py
@@ -746,10 +746,10 @@ def densityBin(fileT,fileS,fileFx,outFile,debug=True,timeint='all',mthout=False)
             volBinzp    = thickBinzp * areaztp
             volBinzi    = thickBinzi * areazti
             # Free memory (!! to be removed if we store these at some point)
-            del(depthBini,  x1Bini,     x2Bini)
-            del(depthBinia, thickBinia, x1Binia, x2Binia)
-            del(depthBinip, thickBinip, x1Binip, x2Binip)
-            del(depthBinii, thickBinii, x1Binii, x2Binii); gc.collect()
+            #del(depthBini,  x1Bini,     x2Bini)
+            #del(depthBinia, thickBinia, x1Binia, x2Binia)
+            #del(depthBinip, thickBinip, x1Binip, x2Binip)
+            #del(depthBinii, thickBinii, x1Binii, x2Binii); gc.collect()
             toziz = timc.clock()
 
             # Compute annual persistence of isopycnal bins (from their thickness): 'persist' array
