4721743e0b8170ceb7e7b3b4527ef37daba670b3
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2014-10-08 13:56:48 -0700

    Clean up and other basins - not yet tested

--- surface_transf.py
+++ surface_transf.py
@@ -65,7 +65,6 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, outFile, debug=True,t
     
     Created on Wed Oct  8 09:15:59 CEST 2014
 
-
     Inputs:
     ------
     - fileTos(time,lat,lon)     - 3D SST array
@@ -283,6 +282,10 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, outFile, debug=True,t
     intWatFlx   = tmp.copy() # integral E-P
     intHeatFlxa = tmp.copy() # integral heat flux Atl
     intWatFlxa  = tmp.copy() # integral E-P Atl
+    intHeatFlxp = tmp.copy() # integral heat flux Pac
+    intWatFlxp  = tmp.copy() # integral E-P Pac
+    intHeatFlxi = tmp.copy() # integral heat flux Ind
+    intWatFlxi  = tmp.copy() # integral E-P Ind
     #
     # Compute area of target grid and zonal sums
     areai = computeArea(loni[:], lati[:])
@@ -292,7 +295,6 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, outFile, debug=True,t
     regridObj = CdmsRegrid(ingrid, outgrid, denflxh.dtype, missing = valmask, regridMethod = 'linear', regridTool = 'esmf')
     # init integration intervals
     dt   = 1./float(N_t) 
-    tostw = regridObj(tos)
 
     # Bin on density grid
     for t in range(N_t):
@@ -304,17 +306,19 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, outFile, debug=True,t
         sost.mask  = maski
         heft.mask  = maski
         empt.mask  = maski
+        tost = maskVal(tost, valmask)
+        sost = maskVal(sost, valmask)
+        heft = maskVal(heft, valmask)
+        empt = maskVal(empt, valmask)
         # Compute density
-        rhon       = eosNeutral(tost.data, sost.data)-1000.
-        rhon.mask  = maski
-        rhon       = maskVal(rhon, valmask)
-        print rhon
+        rhon[t,...] = eosNeutral(tost.data, sost.data) - 1000.
+        rhonl = rhon.data[t,...]
          # Compute buoyancy/density flux as mass fluxes in kg/m2/s (SI unts)
-        #  convwf : kg/m2/s=mm/s -> m/s
+        #  convwf : kg/m2/s = mm/s -> m/s
         convwf = 1.e-3
         pres = tost.data*0.
         denflxh[t,...] = (-sw.alpha(sost.data,tost.data,pres)/sw.cp(sost.data,tost.data,pres))*heft.data
-        denflxw[t,...] = (rhon.data+1000.)*sw.beta(sost.data,tost.data,pres)*sost.data*empt.data*convwf
+        denflxw[t,...] = (rhonl+1000.)*sw.beta(sost.data,tost.data,pres)*sost.data*empt.data*convwf
         denflx [t,...] = denflxh[t,...] + denflxw[t,...]
         denflx [t,...].mask  = maski
         denflxh[t,...].mask  = maski
@@ -329,42 +333,73 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, outFile, debug=True,t
         for ks in range(N_s-1):
             # find indices of points in density bin
             # Global
-            idxbin = npy.argwhere( (rhon.data >= sigrid[ks]) & (rhon.data < sigrid[ks+1]) ).transpose()
+            idxbin = npy.argwhere( (rhonl >= sigrid[ks]) & (rhonl < sigrid[ks+1]) ).transpose()
             idj = idxbin[0] ; idi = idxbin[1]
             transfh[t,ks] = cdu.averager(dflxh[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
             transfw[t,ks] = cdu.averager(dflxw[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
             areabin[t,ks] = cdu.averager(areai[idxbin[0],idxbin[1]], axis=0, action='sum')
             # Basin
-            idxbina = npy.argwhere( (rhon.data*maskAtl >= sigrid[ks]) & (rhon.data*maskAtl < sigrid[ks+1]) ).transpose()
+            idxbina = npy.argwhere( (rhonl*maskAtl >= sigrid[ks]) & (rhonl*maskAtl < sigrid[ks+1]) ).transpose()
             idj = idxbina[0] ; idi = idxbina[1]
             transfha[t,ks] = cdu.averager(dflxh[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
             transfwa[t,ks] = cdu.averager(dflxw[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
             areabina[t,ks] = cdu.averager(areai[idj,idi], axis=0, action='sum')
+            #
+            idxbinp = npy.argwhere( (rhonl*maskPac >= sigrid[ks]) & (rhonl*maskPac < sigrid[ks+1]) ).transpose()
+            idj = idxbinp[0] ; idi = idxbinp[1]
+            transfhp[t,ks] = cdu.averager(dflxh[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
+            transfwp[t,ks] = cdu.averager(dflxw[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
+            areabinp[t,ks] = cdu.averager(areai[idj,idi], axis=0, action='sum')
+            #
+            idxbini = npy.argwhere( (rhonl*maskInd >= sigrid[ks]) & (rhonl*maskInd < sigrid[ks+1]) ).transpose()
+            idj = idxbini[0] ; idi = idxbini[1]
+            transfhi[t,ks] = cdu.averager(dflxh[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
+            transfwi[t,ks] = cdu.averager(dflxw[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
+            areabini[t,ks] = cdu.averager(areai[idj,idi], axis=0, action='sum')
             
         # last bin
-        idxbin = npy.argwhere( (rhon.data >= sigrid[N_s-1])).transpose()
+        # Global
+        idxbin = npy.argwhere( (rhonl >= sigrid[N_s-1])).transpose()
         idj = idxbin[0] ; idi = idxbin[1]
         transfh[t,N_s] = cdu.averager(dflxh[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
         transfw[t,N_s] = cdu.averager(dflxw[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
         areabin[t,N_s] = cdu.averager(areai[idj,idi], axis=0, action='sum')
-        #
-        idxbina = npy.argwhere( (rhon.data*maskAtl >= sigrid[N_s-1])).transpose()
+        # Basins
+        idxbina = npy.argwhere( (rhonl*maskAtl >= sigrid[N_s-1])).transpose()
         idj = idxbina[0] ; idi = idxbina[1]
         transfha[t,N_s] = cdu.averager(dflxh[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
         transfwa[t,N_s] = cdu.averager(dflxw[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
         areabina[t,N_s] = cdu.averager(areai[idj,idi], axis=0, action='sum')
+        # 
+        idxbinp = npy.argwhere( (rhonl*maskPac >= sigrid[N_s-1])).transpose()
+        idj = idxbinp[0] ; idi = idxbinp[1]
+        transfhp[t,N_s] = cdu.averager(dflxh[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
+        transfwp[t,N_s] = cdu.averager(dflxw[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
+        areabinp[t,N_s] = cdu.averager(areai[idj,idi], axis=0, action='sum')
+        # 
+        idxbini = npy.argwhere( (rhonl*maskInd >= sigrid[N_s-1])).transpose()
+        idj = idxbini[0] ; idi = idxbini[1]
+        transfhi[t,N_s] = cdu.averager(dflxh[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
+        transfwi[t,N_s] = cdu.averager(dflxw[idj,idi] * areai[idj,idi], axis=0, action='sum')/del_s[ks]
+        areabini[t,N_s] = cdu.averager(areai[idj,idi], axis=0, action='sum')
         # Total transformation
         transf[t,:] = transfh[t,:] + transfw[t,:]        
         transfa[t,:] = transfha[t,:] + transfwa[t,:]        
+        transfp[t,:] = transfhp[t,:] + transfwp[t,:]        
+        transfi[t,:] = transfhi[t,:] + transfwi[t,:]        
         # domain integrals
         # heat flux (conv W -> PW)
         convt  = 1.e-15
         intHeatFlx [t] = cdu.averager(npy.reshape(dflxh*areai*maski  , (Nji*Nii)), action='sum')*dt*convt
         intHeatFlxa[t] = cdu.averager(npy.reshape(dflxh*areai*maskAtl, (Nji*Nii)), action='sum')*dt*convt
+        intHeatFlxp[t] = cdu.averager(npy.reshape(dflxh*areai*maskPac, (Nji*Nii)), action='sum')*dt*convt
+        intHeatFlxi[t] = cdu.averager(npy.reshape(dflxh*areai*maskInd, (Nji*Nii)), action='sum')*dt*convt
         # fw flux (conv mm -> m and m3/s to Sv)
         convw = 1.e-3*1.e-6
         intWatFlx [t]  = cdu.averager(npy.reshape(dflxw*areai*maski  , (Nji*Nii)), action='sum')*dt*convw
         intWatFlxa[t]  = cdu.averager(npy.reshape(dflxw*areai*maskAtl, (Nji*Nii)), action='sum')*dt*convw
+        intWatFlxp[t]  = cdu.averager(npy.reshape(dflxw*areai*maskPac, (Nji*Nii)), action='sum')*dt*convw
+        intWatFlxi[t]  = cdu.averager(npy.reshape(dflxw*areai*maskInd, (Nji*Nii)), action='sum')*dt*convw
       
     # Wash mask over variables for transformation
     maskt         = mv.masked_values(tos, valmask).mask
@@ -390,6 +425,22 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, outFile, debug=True,t
     transfha      = maskVal(transfha, valmask)
     transfwa      = maskVal(transfwa, valmask)
     transfa       = maskVal(transfa , valmask)
+    #
+    maskin       = mv.masked_values(transfp, valmask).mask
+    transfhp._FillValue = valmask
+    transfwp._FillValue = valmask
+    transfp._FillValue  = valmask
+    transfhp      = maskVal(transfhp, valmask)
+    transfwp      = maskVal(transfwp, valmask)
+    transfp       = maskVal(transfp , valmask)
+    #
+    maskin       = mv.masked_values(transfi, valmask).mask
+    transfhi._FillValue = valmask
+    transfwi._FillValue = valmask
+    transfi._FillValue  = valmask
+    transfhi      = maskVal(transfhi, valmask)
+    transfwi      = maskVal(transfwi, valmask)
+    transfi       = maskVal(transfi , valmask)
        
     #+ create a basins variables (loop on n masks)
 
@@ -410,38 +461,68 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, outFile, debug=True,t
     #
     # Transformation (2D: time, sigma)
     convw = 1.e-6
-    totTransf   = cdm.createVariable(transf*convw , axes = [time, s_axis], id = 'trsftot')
-    hefTransf   = cdm.createVariable(transfh*convw, axes = [time, s_axis], id = 'trsfhef')
-    wfoTransf   = cdm.createVariable(transfw*convw, axes = [time, s_axis], id = 'trsfwfo')
+    totTransf   = cdm.createVariable(transf*convw  , axes = [time, s_axis], id = 'trsftot')
+    hefTransf   = cdm.createVariable(transfh*convw , axes = [time, s_axis], id = 'trsfhef')
+    wfoTransf   = cdm.createVariable(transfw*convw , axes = [time, s_axis], id = 'trsfwfo')
     totTransfa  = cdm.createVariable(transfa*convw , axes = [time, s_axis], id = 'trsftotAtl')
     hefTransfa  = cdm.createVariable(transfha*convw, axes = [time, s_axis], id = 'trsfhefAtl')
     wfoTransfa  = cdm.createVariable(transfwa*convw, axes = [time, s_axis], id = 'trsfwfoAtl')
+    totTransfp  = cdm.createVariable(transfp*convw , axes = [time, s_axis], id = 'trsftotPac')
+    hefTransfp  = cdm.createVariable(transfhp*convw, axes = [time, s_axis], id = 'trsfhefPac')
+    wfoTransfp  = cdm.createVariable(transfwp*convw, axes = [time, s_axis], id = 'trsfwfoPac')
+    totTransfi  = cdm.createVariable(transfi*convw , axes = [time, s_axis], id = 'trsftotInd')
+    hefTransfi  = cdm.createVariable(transfhi*convw, axes = [time, s_axis], id = 'trsfhefInd')
+    wfoTransfi  = cdm.createVariable(transfwi*convw, axes = [time, s_axis], id = 'trsfwfoInd')
     totTransf.long_name   = 'Total transformation'
     totTransf.units       = 'Sv'
     hefTransf.long_name   = 'Heat flux transformation'
     hefTransf.units       = 'Sv'
     wfoTransf.long_name   = 'Water flux transformation'
     wfoTransf.units       = 'Sv'
-    totTransfa.long_name   = 'Atl. Total transformation'
-    totTransfa.units       = 'Sv'
-    hefTransfa.long_name   = 'Atl. Heat flux transformation'
-    hefTransfa.units       = 'Sv'
-    wfoTransfa.long_name   = 'Atl. Water flux transformation'
-    wfoTransfa.units       = 'Sv'
+    totTransfa.long_name  = 'Atl. Total transformation'
+    totTransfa.units      = 'Sv'
+    hefTransfa.long_name  = 'Atl. Heat flux transformation'
+    hefTransfa.units      = 'Sv'
+    wfoTransfa.long_name  = 'Atl. Water flux transformation'
+    wfoTransfa.units      = 'Sv'
+    totTransfp.long_name  = 'Pac. Total transformation'
+    totTransfp.units      = 'Sv'
+    hefTransfp.long_name  = 'Pac. Heat flux transformation'
+    hefTransfp.units      = 'Sv'
+    wfoTransfp.long_name  = 'Pac. Water flux transformation'
+    wfoTransfp.units      = 'Sv'
+    totTransfi.long_name  = 'Ind. Total transformation'
+    totTransfi.units      = 'Sv'
+    hefTransfi.long_name  = 'Ind. Heat flux transformation'
+    hefTransfi.units      = 'Sv'
+    wfoTransfi.long_name  = 'Ind. Water flux transformation'
+    wfoTransfi.units      = 'Sv'
     #
     # Integral heat and emp fux (1D: time)
-    intQFlx   = cdm.createVariable(intHeatFlx, axes = [time], id = 'intQflx')
-    intWFlx   = cdm.createVariable(intWatFlx  , axes = [time], id = 'intWflx')
-    intQFlxa  = cdm.createVariable(intHeatFlxa, axes = [time], id = 'intQflxAtl')
+    intQFlx   = cdm.createVariable(intHeatFlx  , axes = [time], id = 'intQflx')
+    intWFlx   = cdm.createVariable(intWatFlx   , axes = [time], id = 'intWflx')
+    intQFlxa  = cdm.createVariable(intHeatFlxa , axes = [time], id = 'intQflxAtl')
     intWFlxa  = cdm.createVariable(intWatFlxa  , axes = [time], id = 'intWflxAtl')
+    intQFlxp  = cdm.createVariable(intHeatFlxp , axes = [time], id = 'intQflxPac')
+    intWFlxp  = cdm.createVariable(intWatFlxp  , axes = [time], id = 'intWflxPac')
+    intQFlxi  = cdm.createVariable(intHeatFlxi , axes = [time], id = 'intQflxInd')
+    intWFlxi  = cdm.createVariable(intWatFlxi  , axes = [time], id = 'intWflxInd')
     intQFlx.long_name   = 'Integral Surface Heat Flux'
     intQFlx.units       = 'PW'
     intWFlx.long_name   = 'Integral Surface E minus P'
     intWFlx.units       = 'Sv'
-    intQFlxa.long_name   = 'Atl. Integral Surface Heat Flux'
-    intQFlxa.units       = 'PW'
-    intWFlxa.long_name   = 'Atl. Integral Surface E minus P'
-    intWFlxa.units       = 'Sv'
+    intQFlxa.long_name  = 'Atl. Integral Surface Heat Flux'
+    intQFlxa.units      = 'PW'
+    intWFlxa.long_name  = 'Atl. Integral Surface E minus P'
+    intWFlxa.units      = 'Sv'
+    intQFlxp.long_name  = 'Pac. Integral Surface Heat Flux'
+    intQFlxp.units      = 'PW'
+    intWFlxp.long_name  = 'Pac. Integral Surface E minus P'
+    intWFlxp.units      = 'Sv'
+    intQFlxi.long_name  = 'Ind. Integral Surface Heat Flux'
+    intQFlxi.units      = 'PW'
+    intWFlxi.long_name  = 'Ind. Integral Surface E minus P'
+    intWFlxi.units      = 'Sv'
 
     outFile_f.write(rhon)
     outFile_f.write(denFlx)
@@ -453,10 +534,20 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, outFile, debug=True,t
     outFile_f.write(totTransfa)
     outFile_f.write(hefTransfa)
     outFile_f.write(wfoTransfa)
+    outFile_f.write(totTransfp)
+    outFile_f.write(hefTransfp)
+    outFile_f.write(wfoTransfp)
+    outFile_f.write(totTransfi)
+    outFile_f.write(hefTransfi)
+    outFile_f.write(wfoTransfi)
     outFile_f.write(intQFlx)
     outFile_f.write(intWFlx)
     outFile_f.write(intQFlxa)
     outFile_f.write(intWFlxa)
+    outFile_f.write(intQFlxp)
+    outFile_f.write(intWFlxp)
+    outFile_f.write(intQFlxi)
+    outFile_f.write(intWFlxi)
     #
     # File global attributes
     for i in range(0,len(file_dic)):
