4ccd87dd2f186134dd08264864feaf733239fb12
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2016-10-07 09:37:48 +0200

    ciclad testing of ptopsigmaxy computation (optimisation)

--- mme_ave_msk.py
+++ mme_ave_msk.py
@@ -61,6 +61,7 @@ if exper <> 'obs':
     #rootDir = '/Users/ericg/Projets/Density_bining/Prod_density_april15/'
     rootDir = '/Volumes/hciclad/data/Density_binning/Prod_density_april15/Raw/'
     rootDir = '/data/ericglod/Density_binning/Prod_density_april15/Raw/'
+    rootdir = '/work/guilyardi/Prod_density_april15/Raw'
     histDir    = rootDir+'historical'
     histNatDir = rootDir+'historicalNat'
     histMMEOut = rootDir+'mme_hist'
