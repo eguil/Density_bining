3a2543dbf8fe702b40d134f35046560282c14a9f
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2016-10-11 16:31:33 +0200

    mme of 1D raw

--- mme_ave_msk.py
+++ mme_ave_msk.py
@@ -62,7 +62,7 @@ if exper <> 'obs':
     # I/O directories
     #rootDir = '/Users/ericg/Projets/Density_bining/Prod_density_april15/'
     rootDir = '/Volumes/hciclad/data/Density_binning/Prod_density_april15/Raw/'
-    #rootDir = '/data/ericglod/Density_binning/Prod_density_april15/Raw/'
+    rootDir = '/data/ericglod/Density_binning/Prod_density_april15/Raw/'
     #rootdir = '/work/guilyardi/Prod_density_april15/Raw'
     histDir    = rootDir+'historical'
     histNatDir = rootDir+'historicalNat'
