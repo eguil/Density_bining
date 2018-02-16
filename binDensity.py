eca4981dc65cf26b0d7504625358a953b6503e29
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-02-16 11:29:36 +0100

    Testing binDensity for extensive fields

--- binDensity.py
+++ binDensity.py
@@ -703,7 +703,7 @@ def densityBin(fileT,fileS,fileFx,outFile,debug=True,timeint='all',mthout=False)
             ssr[0,:] = ssr[1,:]
             #print 's_s,ssr',s_s[:,ijtest],ssr[:,ijtest]
             inds_bottom = npy.argwhere ( (szmax <= s_s) & (szmax > ssr) ).transpose()
-            bottom_ind = npy.ma.ones(lonN*latN)*0
+            bottom_ind = [0]*(lonN*latN)
             bottom_ind [inds_bottom[1]] = inds_bottom[0]
             print  inds_bottom.shape, inds.shape
             print s_s[inds[0][npy.argwhere (inds[1] == ijtest)],ijtest]
