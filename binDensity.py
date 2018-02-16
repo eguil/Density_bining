138a8e544e6f0018580346e52c7385492ac1eb5c
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-02-16 17:04:03 +0100

    Testing binDensity for extensive issues

--- binDensity.py
+++ binDensity.py
@@ -847,16 +847,16 @@ def densityBin(fileT,fileS,fileFx,outFile,debug=True,timeint='all',mthout=False)
             print 'x3_bin', x3_bin[0,:,j,i]
         if debug and tc == 0:
             # Check integrals/mean on source density grid
-            print '  voltotij0[ijtest], temtotij0[ijtest],saltotij0[ijtest] ',voltotij0[ijtest], temtotij0[ijtest],saltotij0[ijtest]
-            print thick_bin.data[tc,:,ijtest]
-            print thick_bin.data[tc,:,ijtest]*(1-thick_bin.mask[tc,:,ijtest])
+            #print '  voltotij0[ijtest], temtotij0[ijtest],saltotij0[ijtest] ',voltotij0[ijtest], temtotij0[ijtest],saltotij0[ijtest]
+            #print thick_bin.data[tc,:,ijtest]
+            #print thick_bin.data[tc,:,ijtest]*(1-thick_bin.mask[tc,:,ijtest])
             voltotij0 = npy.sum(npy.ma.reshape(thick_bin,(tcdel, N_s+1, latN*lonN)).data[tc,:,:]*(1-npy.ma.reshape(thick_bin,(tcdel, N_s+1, latN*lonN)).mask[tc,:,:]), axis=0)
             temtotij0 = npy.sum(npy.ma.reshape(thick_bin,(tcdel, N_s+1, latN*lonN)).data[tc,:,:]*npy.ma.reshape(x1_bin,(tcdel, N_s+1, latN*lonN)).data[tc,:,:]*(1-npy.ma.reshape(thick_bin,(tcdel, N_s+1, latN*lonN)).mask[tc,:,:]), axis=0)
             saltotij0 = npy.sum(npy.ma.reshape(thick_bin,(tcdel, N_s+1, latN*lonN)).data[tc,:,:]*npy.ma.reshape(x2_bin,(tcdel, N_s+1, latN*lonN)).data[tc,:,:]*(1-npy.ma.reshape(thick_bin,(tcdel, N_s+1, latN*lonN)).mask[tc,:,:]), axis=0)
             voltot = npy.sum(voltotij0*npy.ma.reshape(area,lonN*latN))
             temtot = npy.sum(temtotij0*npy.ma.reshape(area,lonN*latN))/voltot
             saltot = npy.sum(saltotij0*npy.ma.reshape(area,lonN*latN))/voltot
-            print voltotij0[ijtest], temtotij0[ijtest],saltotij0[ijtest]
+            #print voltotij0[ijtest], temtotij0[ijtest],saltotij0[ijtest]
             print '  Total volume in rho coordinates source grid (ref = 1.33 e+18) : ', voltot
             print '  Mean Temp./Salinity in rho coordinates source grid            : ', temtot, saltot
         #
