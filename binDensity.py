75a503cc0b2a2da995f771328e40d52e1fe545ae
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-02-16 11:25:15 +0100

    Testing binDensity for extensive fields

--- binDensity.py
+++ binDensity.py
@@ -703,11 +703,13 @@ def densityBin(fileT,fileS,fileFx,outFile,debug=True,timeint='all',mthout=False)
             ssr[0,:] = ssr[1,:]
             #print 's_s,ssr',s_s[:,ijtest],ssr[:,ijtest]
             inds_bottom = npy.argwhere ( (szmax <= s_s) & (szmax > ssr) ).transpose()
+            bottom_ind = npy.ma.ones(lonN*latN)*0
+            bottom_ind [inds_bottom[1]] = inds_bottom[0]
             print  inds_bottom.shape, inds.shape
             print s_s[inds[0][npy.argwhere (inds[1] == ijtest)],ijtest]
             print s_s[inds_bottom[0][npy.argwhere (inds_bottom[1] == ijtest)],ijtest]
             #inds_bottom = N_s # was N_s -1 with bottom bug Feb 2018
-            zst = npy.tile(z_s[inds_bottom[0],inds_bottom[1]].reshape(lonN*latN), N_s).reshape(lonN*latN,N_s).transpose()
+            zst = npy.tile(z_s[bottom_ind,:].reshape(lonN*latN), N_s).reshape(lonN*latN,N_s).transpose()
             z_s [inds[0],inds[1]] = zst [inds[0],inds[1]]
             #z_s [inds[0],inds[1]] = z_s[inds_bottom[0],inds[1]]
             c1_s[inds[0],inds[1]] = c1_s[inds_bottom[0],inds[1]]
