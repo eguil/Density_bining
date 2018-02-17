3edecaeafd06ffdb84e013c65f7a6b4550bc7039
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-02-17 19:35:17 +0100

    Testing binDensity for extensive issues

--- binDensity.py
+++ binDensity.py
@@ -613,6 +613,7 @@ def densityBin(fileT,fileS,fileFx,outFile,debug=True,timeint='all',mthout=False)
             # Vertical integral of x3_content from bottom
             x3cumulz = npy.ma.ones([depthN, latN*lonN])*valmask
             for k in range(depthN-1,0,-1):
+                print k
                 print npy.ma.cumsum(x3_content[k:depthN,:], axis=0).shape
                 print npy.ma.cumsum(x3_content[k:depthN,ijtest], axis=0)
                 print x3cumulz[k,:].shape
