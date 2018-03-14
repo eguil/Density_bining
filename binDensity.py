5b4a7d4f31c3d925e2617df1dc6a19bc74616d44
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-03-14 11:25:22 +0100

    binDensity OK for extensive field interpolation + integral conservation

--- binDensity.py
+++ binDensity.py
@@ -519,7 +519,7 @@ def densityBin(fileT,fileS,fileFx,outFile,debug=True,timeint='all',mthout=False)
     # Compute level thickness in source z grid (lev_thickt is a replicate for 3D matrix computation)
     lev_thick     = npy.roll(z_zw,-1)-z_zw
     lev_thick[-1] = lev_thick[-2]
-    print 'lev_thick,z_zw ',lev_thick,z_zw
+    #print 'lev_thick,z_zw ',lev_thick,z_zw
     lev_thickt    = npy.swapaxes(mv.reshape(npy.tile(lev_thick,lonN*latN),(lonN*latN,depthN)),0,1)
 
     # testing
