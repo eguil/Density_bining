3536ed5ed28ad52d04684c833f8ea52b434b361f
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-05-09 17:13:05 +0200

    testing msf

--- binDensity.py
+++ binDensity.py
@@ -773,7 +773,6 @@ def densityBin(fileT,fileS,fileV,fileFx,outFile,debug=True,timeint='all',mthout=
                 print ' bottom correction', bottom_ind[0,ijtest]
                 print c3ders[bottom_ind[0,ijtest],bottom_ind[1,ijtest]], c3_s[0,ijtest], c3_s[bottom_ind[0,ijtest],bottom_ind[1,ijtest]], c3_s[bottom_ind[0,ijtest]-1,bottom_ind[1,ijtest]]
             # Densest value of derivative on s grid x3ders should be equal to c3_s
-            c3ders=c3_s*1.
             c3ders[bottom_ind[0],bottom_ind[1]] = c3_s[0,:] - c3_s[bottom_ind[0]-1,bottom_ind[1]]
             c3_s = c3ders
             if debug and t == 0:
