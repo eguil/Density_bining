74cd8fa9264d5ee62074b2ba06890bf88c5ba3d3
Eric Guilyardi <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-09-12 18:08:34 +0200

    Surface transf plotting

--- matplotlib/surf_transf_plot.py
+++ matplotlib/surf_transf_plot.py
@@ -9,7 +9,7 @@ Make surface transformation plots
 """
 from netCDF4 import Dataset as open_ncfile
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy as npy
 
 # -------------------------------------------------------------------------------
 #                                Define work
@@ -18,22 +18,38 @@ import numpy as np
 #inDir = '/Users/ericg/Projets/Density_bining/Nudge_CMIP6_IPSL/'
 inDir = '/data/ericglod/Runs_nudges_sept2018'
 
-file1  = 'CM6-pace-TSTr7fgT_1950_2009_mean_transf_north.nc'
+run1 = 'CM6-pace-TSTr7fgT'
+file1  = run1+'_1950_2009_mean_transf_north.nc'
 
 #
 # -- Open netcdf files
-nc1 = open_ncfile(inDir + '/' + file2dh)
+nc1 = open_ncfile(inDir + '/' + file1)
 
 # -- Read variables for NorthAtl
-trfatltot1 = nc1.variables['trsftotAtl'][:]
-trfatlhef1 = nc1.variables['trsfhefAtl'][:]
-trfatlwfo1 = nc1.variables['trsfwfoAtl'][:]
+trfatltot1 = npy.ma.average(nc1.variables['trsftotAtl'][:,:].squeeze(),axis=0)
+trfatlhef1 = npy.ma.average(nc1.variables['trsfhefAtl'][:,:].squeeze(),axis=0)
+trfatlwfo1 = npy.ma.average(nc1.variables['trsfwfoAtl'][:,:].squeeze(),axis=0)
 
 # axis
 levr = nc1.variables['rhon'][:]
+sigmin = 22
+sigmax = 30
+trfmin = -15
+trfmax = 30
 
+plt.axis([sigmin, sigmax, trfmin, trfmax])
 
-plt.plot(levr, trfatltot1)
+plt.plot(levr, trfatltot1, c = 'b', label = run1)
+plt.plot(levr, trfatlhef1, c = 'b', linestyle ='--')
+plt.plot(levr, trfatlwfo1, c = 'b', linestyle =':')
+
+plt.xlabel('sigma_n', fontsize=14)
+plt.ylabel('Tranformation(Sv)', fontsize=14)
+plt.hlines(0.,sigmin, sigmax)
+
+plt.legend(loc='upper left', title='', fontsize=10)
+
+plt.text(23, 32, 'Surface transformation North Atl.', fontsize=14, fontweight='bold')
 
 plt.show()
 
