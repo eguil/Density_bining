5f7d63cf3ce7596c1de40cb076665cd04c245dd8
Eric Guilyardi <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-09-12 16:09:44 +0200

    Surface transf domain option (North/South)

--- surface_transf.py
+++ surface_transf.py
@@ -337,7 +337,7 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, varNames, outFile, de
         gt.close()
         # Reduce domain to North/South ?
         if domain == 'north':
-            lati2d = npy.tile(lati, Nii).reshape(Nji, Nii).transpose
+            lati2d = npy.tile(lati, Nii).reshape(Nji, Nii).transpose()
             print lati2d.shape
             print maskAtl.shape
             print lati2d[0,:]
@@ -349,7 +349,7 @@ def surfTransf(fileFx, fileTos, fileSos, fileHef, fileWfo, varNames, outFile, de
             maskInd[indn[0],indn[1]] = False
             print maskAtl[0,:]
         elif domain == 'south':
-            lati2d = npy.tile(lati, Nii).reshape(Nii,Nji)
+            lati2d = npy.tile(lati, Nii).reshape(Nji,Nii).transpose()
             print lati2d.shape
             indn = npy.argwhere(lati2d >= 0).transpose()
             maskAtl[indn[0],indn[1]] = False
