145eab1fae1c3e71e8d2ae977d49ebe48261c287
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-09-12 17:27:16 +0200

    Surface transf plotting

---
+++ matplotlib/surf_transf_plot.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Python matplotlib
+Make surface transformation plots
+
+(c) Eric Guilyardi Sept. 2018
+
+"""
+from netCDF4 import Dataset as open_ncfile
+import matplotlib.pyplot as plt
+import numpy as np
+
+# -------------------------------------------------------------------------------
+#                                Define work
+# -------------------------------------------------------------------------------
+
+inDir = '/Users/ericg/Projets/Density_bining/Nudge_CMIP6_IPSL/'
+
+file1  = 'CM6-pace-TSTr7fgT_1950_2009_mean_transf_north.nc'
+
+#
+# -- Open netcdf files
+nc1 = open_ncfile(inDirh + '/' + file2dh)
+
+# -- Read variables for NorthAtl
+trfatltot1 = nc1.variables['trsftotAtl'][:]
+trfatlhef1 = nc1.variables['trsfhefAtl'][:]
+trfatlwfo1 = nc1.variables['trsfwfoAtl'][:]
+
+# axis
+levr = nc1.variables['rhon'][:]
+
+
+plt.plot(levr, trfatltot1)
+
+plt.show()
+
