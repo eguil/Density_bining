895a0d86bdfed94862883f7256f3b2a0dd49549f
Eric Guilyardi <Eric.Guilyardi@locean-ipsl.upmc.fr>
2018-09-13 13:35:16 +0200

    Surface transf plotting

--- matplotlib/surf_transf_plot.py
+++ matplotlib/surf_transf_plot.py
@@ -18,18 +18,22 @@ import numpy as npy
 #inDir = '/Users/ericg/Projets/Density_bining/Nudge_CMIP6_IPSL/'
 inDir = '/data/ericglod/Runs_nudges_sept2018/'
 
+# use cdo yseasmean to compute seasonal climatology
 run1 = 'historical_r1i1p1f1'
 file1  = 'IPSL-CM6A-LR_historical_r1i1p1f1_gn_1950_2009_seasmean_transf_north40.nc'
 run2 = 'CM6-pace-TSTr7fgT'
 file2  = run2+'_1950_2009_seasmean_transf_north40.nc'
 run3 = 'CM6-pace-TSTr7vg'
 file3  = run3+'_1950_2009_seasmean_transf_north40.nc'
+run4 = 'CM6-pace-TSTr8vgS0'
+file4  = run4+'_1950_2009_seasmean_transf_north40.nc'
 
 #
 # -- Open netcdf files
 nc1 = open_ncfile(inDir + run1 + '/' + file1)
 nc2 = open_ncfile(inDir + run2 + '/' + file2)
 nc3 = open_ncfile(inDir + run3 + '/' + file3)
+nc4 = open_ncfile(inDir + run4 + '/' + file4)
 
 # -- Read variables for North Atl (anual mean from cdo yearmean on monthly data)
 
@@ -44,6 +48,10 @@ trfatlwfo2 = nc2.variables['trsfwfoAtl'][0,:].squeeze()
 trfatltot3 = nc3.variables['trsftotAtl'][0,:].squeeze()
 trfatlhef3 = nc3.variables['trsfhefAtl'][0,:].squeeze()
 trfatlwfo3 = nc3.variables['trsfwfoAtl'][0,:].squeeze()
+
+trfatltot4 = nc4.variables['trsftotAtl'][0,:].squeeze()
+trfatlhef4 = nc4.variables['trsfhefAtl'][0,:].squeeze()
+trfatlwfo4 = nc4.variables['trsfwfoAtl'][0,:].squeeze()
 
 
 # axis
@@ -67,13 +75,17 @@ plt.plot(levr, trfatltot3, c = 'r', label = run3)
 plt.plot(levr, trfatlhef3, c = 'r', linestyle ='--')
 plt.plot(levr, trfatlwfo3, c = 'r', linestyle =':')
 
+plt.plot(levr, trfatltot4, c = 'g', label = run4)
+plt.plot(levr, trfatlhef4, c = 'g', linestyle ='--')
+plt.plot(levr, trfatlwfo4, c = 'g', linestyle =':')
+
 plt.xlabel('sigma_n', fontsize=14)
 plt.ylabel('Tranformation(Sv)', fontsize=14)
 plt.hlines(0.,sigmin, sigmax)
 
 plt.legend(loc='upper left', title='', fontsize=10)
 
-plt.text(23, trfmax+3, 'IPSL-CM6A-LR 1950-2009 DJF Surface transformation North Atl. > 40N', fontsize=14, fontweight='bold')
+plt.text(22.5, trfmax+2, 'IPSL-CM6A-LR 1950-2009 DJF Surface transformation North Atl. > 40N', fontsize=14, fontweight='bold')
 
 plt.show()
 
