70f5d7894ce006c2046f176f3b98de108e854907
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2019-03-23 09:25:04 +0100

    Modifications to account for negative valmask (such as in obs)

--- binDensity.py
+++ binDensity.py
@@ -648,11 +648,11 @@ def densityBin(fileT,fileS,fileFx,fileV='none',outFile='out.nc',debug=True,timei
         so      = fs('so'    , time = slice(trmin,trmax))
         # Correct for mask value if needed
         if corrmask:
-            print thetao.shape
+            print thetao.data.shape
             print ' thetao before correct :',thetao.data[0,:,jtest,itest]
             print ' mask:',thetao.mask[0,:,jtest,itest]
             thetao = npy.ma.filled(thetao, fill_value=valmask)
-            print thetao.shape
+            print thetao.data.shape
             print ' thetao after correct :',thetao.data[0,:,jtest,itest]
             so     = maskVal    (so,    valmaski)
             so     = maskValCorr(so,    valmaski,valmask)
